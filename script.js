ea0d425e1fe2d77250d85c04435ec41628a542ed
MCoppinDev <mattcoppinger@protonmail.com>
2022-01-20 23:41:43 -0500

    made changes to script.js and scriptHelper.js.

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
+  let listedPlanetsResponse = myFetch();
+  listedPlanetsResponse.then(function (json) {
+    
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
-});
+    console.log(json);
+  });
+
+  const form = document.querySelector("form");
+
+    form.addEventListener("submit", function (event) {
+      let pilotNameInput = document.querySelector("input[name=pilotName]").value;
+      let copilotNameInput = document.querySelector ("input[name=copilotName]").value;
+      let fuelLevelInput = document.querySelector("input[name=fuelLevel]").value;
+      let cargoMassInput = document.querySelector("input[name=cargoMass]").value;
+      let faultyItems = document.querySelector("#faultyItems");
+      event.preventDefault();
+      formSubmission(document,faultyItems,pilotNameInput,copilotNameInput,fuelLevelInput,cargoMassInput);
+    });
+  });
